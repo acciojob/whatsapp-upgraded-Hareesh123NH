b135b28a8b4e1470d71135ddeacdaeecdec60f50
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-08-05 23:47:09 +0530

    test cases

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -22,8 +22,8 @@ public class WhatsappRepository {
         this.senderMap = new HashMap<Message, User>();
         this.adminMap = new HashMap<Group, User>();
         this.userMobile = new HashSet<>();
-        this.customGroupCount = 1;
-        this.messageId = 1;
+        this.customGroupCount = 0;
+        this.messageId = 0;
     }
 
     public String createUser(String name, String mobile) throws Exception {
