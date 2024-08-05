e652320003ed437af5ce0ad38eb2d518acd926a6
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-08-05 23:41:52 +0530

    Constucter

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,7 +6,10 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
-
+     public Message(int id,String content){
+         this.id=id;
+         this.content=content;
+     }
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
